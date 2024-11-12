398152eef59f90634e9c03287e4b602609901423
yulealmeida <yule.alme2@gmail.com>
2024-11-12 20:22:37 -0300

    Database seed update

--- src/main/java/com/devsuperior/dslist/entities/GameList.java
+++ src/main/java/com/devsuperior/dslist/entities/GameList.java
@@ -5,7 +5,7 @@ import jakarta.persistence.*;
 import java.util.Objects;
 
 @Entity
-@Table(name = "tb_name_list")
+@Table(name = "tb_game_list")
 public class GameList {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
